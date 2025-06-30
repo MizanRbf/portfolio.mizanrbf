308d6a3fb5f8f1befe53997b0a3d6e608197b554
MizanRbf <mizanrbf@gmail.com>
2025-06-30 18:17:34 +0600

    education section updated with text color change

--- src/Components/Education/EducationCoponent.jsx
+++ src/Components/Education/EducationCoponent.jsx
@@ -13,30 +13,28 @@ const Section = ({ children }) => (
 
 const EducationComponent = () => {
   return (
-    <section id="education" className="py-12 text-black dark:text-white">
+    <section id="education" className="py-12 text-white">
       <div className="space-y-6">
         {/* University */}
         <Section>
-          <div className="bg-secondary text-black p-5 rounded-md shadow ">
+          <div className="bg-secondary p-5 rounded-md shadow ">
             <h3 className="text-xl text-white font-semibold">
               University of Rajshahi
             </h3>
             <p className="text-sm text-gray-400">
               Bachelor in Arabic Language and Literature
             </p>
-            <p className="text-sm text-gray-600 dark:text-gray-400">
-              2023 – Present
-            </p>
+            <p className="text-sm text-gray-400">2023 – Present</p>
           </div>
         </Section>
 
         {/* Optional: Another education */}
         <Section>
           <div className="bg-secondary p-5 rounded-md shadow">
-            <h3 className="text-xl font-semibold">Programming Hero</h3>
-            <p className="text-sm text-gray-600 dark:text-gray-400">
-              Web Development Course
-            </p>
+            <h3 className="text-xl font-semibold text-white">
+              Programming Hero
+            </h3>
+            <p className="text-sm text-gray-400">Web Development Course</p>
             <p className="text-sm text-gray-400">2024 – Present</p>
           </div>
         </Section>
